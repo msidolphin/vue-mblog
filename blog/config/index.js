ecd7262237bcefb158a3f7888f7e24f9bbd0e3f8
msidolphin <OoMass-EffectoO@hotmail.com>
2018-05-13 20:05:55 +0800

    修改生产环境配置；添加favicon；增加网页关键词和描述

--- blog/config/index.js
+++ blog/config/index.js
@@ -58,7 +58,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '', // /
 
     /**
      * Source Maps
@@ -79,6 +79,7 @@ module.exports = {
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
+    bundleAnalyzerReport: process.env.npm_config_report,
+    port: 9000
   }
 }
